1f1f15916f49106db13fed763d9d2cb78e5331c7
Nonato Sales <nrdesales@hotmail.com>
2016-05-04 18:09:56 -0300

    Seção 2 - Baterias Inclusas - Algumas coisas que vem com o Django  - aula 15

--- simplemooc/courses/models.py
+++ simplemooc/courses/models.py
@@ -2,6 +2,13 @@
 
 from django.db import models
 
+class CourseManager(models.Manager):
+
+	def search(self, query):
+		return self.get_queryset().filter(
+			models.Q(name__icontains=query | models.Q(description__icontains=query))
+		)
+
 class Course(models.Model):
 
 	name = models.CharField('Nome', max_length=100)
@@ -11,3 +18,9 @@ class Course(models.Model):
 	image = models.ImageField(upload_to='courses/images', verbose_name='Imagem', null=True, blank=True)
 	created_at = models.DateTimeField('Criado em', auto_now_add=True)
 	update_at = models.DateTimeField('Atualizado em', auto_now=True)
+
+	def __str__(self):
+
+		return self.name
+
+	objects = CourseManager()
