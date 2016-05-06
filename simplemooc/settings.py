0b445e0ce3ab8c764c5a898d1c7c047232e83768
Nonato Sales <nrdesales@hotmail.com>
2016-05-06 20:06:50 -0300

    Seção 3 - Visão Externa dos Cursos - aula 31

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -124,3 +124,17 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'simplemooc', 'media')
 MEDIA_URL = '/media/'
+
+# Email configuration
+
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = 'Nome <nrdesales@gmail.com>'
+EMAIL_USE_TLS = True
+#EMAIL_USE_SSL = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'nrdesales@gmail.com'
+EMAIL_HOST_PASSWORD = '3sdr@s2014*'
+EMAIL_PORT = 587
+
+CONTACT_EMAIL = EMAIL_HOST_USER
