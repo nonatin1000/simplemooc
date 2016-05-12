f0b73b91ef122fd0c3249d4b35d3a9ce3905b310
Nonato Sales <nrdesales@hotmail.com>
2016-05-12 19:01:28 -0300

    Seção 5 - Acessando o Curso - aula 59

--- simplemooc/courses/models.py
+++ simplemooc/courses/models.py
@@ -60,3 +60,34 @@ class Enrollment(models.Model):
 		verbose_name = 'Inscrição'
 		verbose_name_plural = 'Inscrições'
 		unique_together=(('user', 'course'),) # This is a tuple of tuples that must be unique when considered together. It’s used in the Django admin and is enforced at the database level (i.e., the appropriate UNIQUE statements are included in the CREATE TABLE statement).
+
+class Announcement(models.Model):
+
+	course = models.ForeignKey(Course, verbose_name='Curso', related_name="announcement")
+	title = models.CharField('Título', max_length=100)
+	content = models.TextField('Conteúdo')
+
+	created_at = models.DateTimeField('Criado em', auto_now_add=True)
+	update_at = models.DateTimeField('Atualizado em', auto_now=True)
+
+	def __str__(self):
+		return self.title
+
+	class Meta:
+		verbose_name = 'Anúncio'
+		verbose_name_plural = 'Anúncios'
+		ordering = ['-created_at']
+
+class Comment(models.Model):
+
+	announcement = models.ForeignKey(Announcement, verbose_name="Anúncio", related_name="comments")
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Usuário', related_name='comments')
+	comment = models.TextField('Comentário')
+
+	created_at = models.DateTimeField('Criado em', auto_now_add=True)
+	update_at = models.DateTimeField('Atualizado em', auto_now=True)
+
+	class Meta:
+		verbose_name = 'Comentário'
+		verbose_name_plural = 'Comentários'
+		ordering = ['created_at']
