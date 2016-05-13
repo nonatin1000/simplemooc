e02684a3774f1255ebfa082d172b5fce146507e1
Nonato Sales <nrdesales@hotmail.com>
2016-05-13 18:25:46 -0300

    Seção 5 - Acessando o Curso - aula 65

--- simplemooc/courses/admin.py
+++ simplemooc/courses/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Course, Enrollment, Announcement, Comment
+from .models import Course, Enrollment, Announcement, Comment, Lesson, Material
 
 class CourseAdmin(admin.ModelAdmin):
 
@@ -8,5 +8,19 @@ class CourseAdmin(admin.ModelAdmin):
 	search_fields = ['name', 'slug']
 	prepopulated_fields = {'slug': ('name', )}
 
+class MaterialInLineAdmin(admin.StackedInline):
+	
+	model = Material
+
+class LessonAdmin(admin.ModelAdmin):
+	list_display = ['name', 'number', 'course', 'release_date']
+	search_fields = ['name', 'description']
+	list_filter = ['created_at']
+
+	inlines = [
+		MaterialInLineAdmin
+	]
+
 admin.site.register(Course, CourseAdmin)
-admin.site.register([Enrollment, Announcement, Comment])
+admin.site.register([Enrollment, Announcement, Comment, Material])
+admin.site.register(Lesson, LessonAdmin)
